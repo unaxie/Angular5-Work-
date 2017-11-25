4e7c2346cc750eaf1b767d83f63f413bd1015243
unaxie <una416@gmail.com>
2017-11-25 11:45:28 +0800

    任務8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,22 @@ import { Component } from "@angular/core";
 export class AppComponent {
   inputHint = "What needs to be done? 呀蛤～";
   todos = [];
+  todo = '';
 
-  addTodos(element: HTMLInputElement) {
-    // console.log(target.value);
-    if (element.value) {
-      this.todos.push(element.value);
+  // addTodos(element: HTMLInputElement) {
+  //   // console.log(target.value);
+  //   if (element.value) {
+  //     this.todos.push(element.value);
+  //   }
+  // }
+  addTodos() {
+    if (this.todo) {
+      this.todos.push(this.todo);
+      let newTodo = {
+        text : this.todo,
+        done : '',
+      };
     }
+    this.todo = '';
   }
 }
