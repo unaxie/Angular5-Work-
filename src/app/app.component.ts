f7645311da3077344ed5df67aab24b745aa90876
unaxie <una416@gmail.com>
2017-11-25 11:26:30 +0800

    第＝種方式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  inputHint= 'What needs to be done? 呀蛤～';
+  inputHint = "What needs to be done? 呀蛤～";
   todos = [];
 
-  addTodos(target:HTMLInputElement) {
-    console.log(target.value);
-    this.todos.push(target.value);
+  addTodos(element: HTMLInputElement) {
+    // console.log(target.value);
+    if (element.value) {
+      this.todos.push(element.value);
+    }
   }
 }
